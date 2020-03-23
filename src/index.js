8bfdc652d6d2e7b275d8043f23d825083fd5457e
mbuehrer <Moritz96>
2020-03-23 20:36:28 +0100

    revert of changes due to push to wrong repo

--- src/index.js
+++ src/index.js
@@ -7,18 +7,9 @@ import deDE from 'antd/es/locale/de_DE';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-//Redux
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './reducers';
-
-const store = createStore(rootReducer);
-
 ReactDOM.render(
   <ConfigProvider locale={deDE}>
-    <Provider store={store}>
       <App />
-    </Provider>
   </ConfigProvider>
   ,
   document.getElementById('root')
